563c523ace0f540f83ef234a526cf49521ae4665
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-05-18 18:13:06 +0200

    Added sockeIo-Client
    Only connects atm
    Still has to do everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { socket } from "./socket";
 import NavBar from "./components/NavBar/NavBar";
 import SideBarConversations from "./components/SideBarConversations/SideBarConversations";
 import WindowConversation from "./components/WindowConversation/WindowConversation";
 
 function App() {
+  socket.on("connect", () => console.log("connecté"));
   return (
     <div className="App">
       <NavBar />
