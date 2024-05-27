ebd5fee159d4be7d58517c3f94cdf4cb70cdee36
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-05-27 16:36:07 +0200

    fix .env firebase config
    Hid firebase config in .env file
    Created .env example file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
-
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
