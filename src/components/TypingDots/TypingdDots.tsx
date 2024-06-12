4254f2f09f111c8d090a4717b2e571ed11dfb4a2
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-06-12 14:32:28 +0200

    feat : message typing animation component

---
+++ src/components/TypingDots/TypingdDots.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TypingDots.css";
+
+function TypingDots() {
+  return (
+    <div className="typing">
+      <div className="typing__dot"></div>
+      <div className="typing__dot"></div>
+      <div className="typing__dot"></div>
+    </div>
+  );
+}
+
+export default TypingDots;
