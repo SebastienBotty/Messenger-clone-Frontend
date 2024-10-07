51a195b55a2b228d844d0e693d0ec45c870a99a3
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-10-07 21:45:50 +0200

    change: created file containing utility function
    Removed timeSince function from component to put it in a utility function to make it reusable everywhere

---
+++ src/functions/time.ts
@@ -0,0 +1,32 @@
+export const timeSince = (date: Date): string => {
+  const now = new Date();
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  const intervals = [
+    { label: "semaine", seconds: 604800 },
+    { label: "jour", seconds: 86400 },
+    { label: "heure", seconds: 3600 },
+    { label: "minute", seconds: 60 },
+  ];
+
+  for (const interval of intervals) {
+    const count = Math.floor(seconds / interval.seconds);
+    if (count >= 1) {
+      switch (interval.label) {
+        case "minute":
+          return count === 1 ? "1m" : `${count}min`;
+        case "heure":
+          return count === 1 ? "1h" : `${count}h`;
+        case "jour":
+          return count === 1 ? "1j" : `${count}j`;
+        case "semaine":
+          return count === 1 ? "1sem" : `${count}sem`;
+
+        default:
+          return "Il y a un certain temps";
+      }
+    }
+  }
+
+  return "A l'instant";
+};
