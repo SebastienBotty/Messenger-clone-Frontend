5dae50d4077d3eb88c708febf5628b99ddf8e47d
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-02-16 13:01:44 +0100

    fixed msg link color text

--- src/components/Utiles/LinkFormatter/LinkFormatter.tsx
+++ src/components/Utiles/LinkFormatter/LinkFormatter.tsx
@@ -1,18 +1,13 @@
+import "./LinkFormatter.css";
+
 export function LinkFormatter({ text }: { text: string }) {
-  const urlRegex =
-    /\b((?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/\S*)?)\b/g;
+  const urlRegex = /\b((?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/\S*)?)\b/g;
 
   const parts = text.split(urlRegex).map((part, index) => {
     if (urlRegex.test(part)) {
       const url = part.startsWith("http") ? part : `http://${part}`;
       return (
-        <a
-          key={index}
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "white" }}
-        >
+        <a key={index} href={url} target="_blank" rel="noopener noreferrer">
           {part}
         </a>
       );
