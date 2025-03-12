b30b331f89c67320882fbebfa1e53770cc5edbb9
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-03-12 01:14:30 +0100

    added not messenger slogan to auth page

--- src/screens/userNotLogged/userNotLogged.tsx
+++ src/screens/userNotLogged/userNotLogged.tsx
@@ -8,6 +8,10 @@ function UserNotLogged() {
 
   return (
     <div className="auth-container">
+      <div className="auth-slogan">
+        <h1>Not Messenger</h1>
+        <p>Works like messenger, looks like messenger but it's Not messenger</p>
+      </div>
       <div className="auth-card">
         <div className="auth-header">
           <div
