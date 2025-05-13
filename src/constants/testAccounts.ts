d6b2ec6a688aaddb420a3a2251f077beb32f57b8
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-05-13 20:04:19 +0200

    removed some test account

--- src/constants/testAccounts.ts
+++ src/constants/testAccounts.ts
@@ -16,27 +16,11 @@ export const comptes = [
     password: "Test123",
   },
   {
-    username: "Loulou@hotmail.com",
-    password: "Test123",
-  },
-  {
-    username: "Thomiche@hotmail.com",
-    password: "Test123",
-  },
-  {
     username: "Seb@hotmail.com",
     password: "Test123",
   },
   {
     username: "Floriss@hotmail.com",
-    password: "Test123",
-  },
-  {
-    username: "Robin@hotmail.com",
-    password: "Test123",
-  },
-  {
-    username: "Horaxy@hotmail.com",
     password: "Test123",
   },
 ];
